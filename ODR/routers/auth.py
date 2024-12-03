c6b0e98fd787f025940ff45600915f5af56c9dc4
Oliwier Kossak <oliwierkossak109@gmail.com>
2024-12-03 18:13:11 +0100

    added hashed password

--- ODR/routers/auth.py
+++ ODR/routers/auth.py
@@ -4,11 +4,14 @@ from models.user import User
 from pydantic import BaseModel, Field
 from typing import Annotated, Optional
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
 
 router = APIRouter(
     prefix='/auth',
     tags=['auth']
 )
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 class CreateUserRequest(BaseModel):
     email:str
@@ -23,7 +26,14 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 @router.post("/create_user", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
-    create_user_model = User(**create_user_request.dict())
+    create_user_model = User(
+        email=create_user_request.email,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        hashed_password=bcrypt_context.hash(create_user_request.hashed_password),
+        is_active=True,
+        role=create_user_request.role
+    )
     db.add(create_user_model)
     db.commit()
 
