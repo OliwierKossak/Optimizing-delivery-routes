0cf2337d2dcfd37ba88dfd4c971a9047a6d9034f
Oliwier Kossak <oliwierkossak109@gmail.com>
2024-12-10 07:17:34 +0100

    create access token

--- ODR/routers/auth.py
+++ ODR/routers/auth.py
@@ -1,3 +1,5 @@
+from datetime import timedelta, datetime, timezone
+
 from database import get_db
 from fastapi import APIRouter, HTTPException, status, Depends, Path
 from models.user import User
@@ -7,11 +9,15 @@ from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm
 from models.user import User
+from jose import jwt
 
 router = APIRouter(
     prefix='/auth',
     tags=['auth']
 )
+
+SECRET_KEY = '04337a2119b07de58c0108e55fc4ca9e0ef35cdb5d975b8275d4a86e353fc987'
+ALGORYTHIM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -31,7 +37,13 @@ def authenticate_user(username: str, password: str, db):
         return False
     if not bcrypt_context.verify(password, user.hashed_password):
         return False
-    return True
+    return user
+
+def create_access_token(email: str, id: int, expires_delta: timedelta):
+    encode = {'sub': email, 'id': id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORYTHIM)
 
 @router.post("/create_user", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
@@ -52,4 +64,5 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return 'Failed Authentication'
-    return 'sucess'
+    token = create_access_token(user.email, user.id, timedelta(minutes=20))
+    return token
