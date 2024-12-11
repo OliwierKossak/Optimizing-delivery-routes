aeec33f88c29150d14a6d1e25ce60d758102e03d
Oliwier Kossak <oliwierkossak109@gmail.com>
2024-12-11 08:37:35 +0100

    resolve changes problems

--- ODR/routers/auth.py
+++ ODR/routers/auth.py
@@ -1,19 +1,26 @@
+from datetime import timedelta, datetime, timezone
+
 from database import get_db
 from fastapi import APIRouter, HTTPException, status, Depends, Path
-from models.user import User
 from pydantic import BaseModel, Field
 from typing import Annotated, Optional
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from models.user import User
+from jose import jwt, JWTError
+
 
 router = APIRouter(
     prefix='/auth',
     tags=['auth']
 )
 
+SECRET_KEY = '04337a2119b07de58c0108e55fc4ca9e0ef35cdb5d975b8275d4a86e353fc987'
+ALGORYTHIM = 'HS256'
+
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 class CreateUserRequest(BaseModel):
     email:str
@@ -22,6 +29,10 @@ class CreateUserRequest(BaseModel):
     hashed_password: str
     is_active: bool
     role: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
@@ -54,7 +65,6 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         print("JWTerror token")
         raise HTTPException(status_code=401, detail='Could not validate user.')
 
-
 @router.post("/create_user", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = User(
@@ -68,10 +78,11 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     db.add(create_user_model)
     db.commit()
 
-@router.post("/token")
+@router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                                  db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        return 'Failed Authentication'
-    return 'sucess'
+        raise HTTPException(status_code=401, detail='Could not validate user.')
+    token = create_access_token(user.email, user.id, timedelta(minutes=20))
+    return {'access_token': token, 'token_type': 'bearer'}
